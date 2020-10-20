f409fa65df913e2f996e27ddc23a9f7fe5c513b3
Rezappli <66002646+Rezappli@users.noreply.github.com>
2020-10-20 16:27:45 +0200

    V1

--- app/src/main/java/com/example/droit/MainActivity.java
+++ app/src/main/java/com/example/droit/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.droit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button taux1;
+    Button taux2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        taux1 = findViewById(R.id.taux1);
+        taux2 = findViewById(R.id.taux2);
+
+        taux2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
