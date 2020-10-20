14e5b4de36bae6ed4c36f061008f653a633c1a10
Rezappli <66002646+Rezappli@users.noreply.github.com>
2020-10-20 15:57:39 +0200

    V1

---
+++ app/src/main/java/com/example/droit/PeriodeProbatoireActivity.java
@@ -0,0 +1,14 @@
+package com.example.droit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PeriodeProbatoireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_periode_probatoire);
+    }
+}
